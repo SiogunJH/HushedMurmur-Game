e795f860cfd5af201e9b5de37c908f5592958082
SiogunJH <homarofficial@gmail.com>
2024-04-30 23:50:23 +0200

    Screech cycle
    screech goes through the cycle of available->performinf->on cooldown, while being used

--- Assets/Scripts/Player/PlayerScreech.cs
+++ Assets/Scripts/Player/PlayerScreech.cs
@@ -1,49 +1,89 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerScreech : MonoBehaviour
 {
+    [Header("Screech Settings")]
     [SerializeField]
     float screechDuration = 0.6f;
-
     [SerializeField]
-    GameObject screechEffectVP;
+    float screechCooldownDuration = 0.5f;
+    float screechEffectAppearanceSpeed = 0.2f;
+    float screechEffectDisappearanceSpeed = 1f;
+    Vector3 screechEffectDisappearancePos = new Vector3(0, -0.5f, 0);
+
+    [HideInInspector]
+    public ScreechStatus currentScreechStatus = ScreechStatus.Available;
+
+    [Header("Other")]
+    [SerializeField]
+    GameObject screechEffectVolume;
+    [SerializeField]
+    GameObject playerCamera;
 
     void Start()
     {
-        if (screechEffectVP == null) Debug.LogWarning($"Missing [Screech Effect VP] reference in {gameObject.name}");
+        if (screechEffectVolume == null) Debug.LogWarning($"Missing [Screech Effect Volume] reference in {gameObject.name}");
+        if (playerCamera == null) Debug.LogWarning($"Missing [Player Camera] reference in {gameObject.name}");
     }
 
 
     public void OnScreech(InputAction.CallbackContext context)
     {
-        if (context.started) OnScreechStart();
+        // Skip multiple callouts
+        if (!context.performed) return;
+
+        // Do not allow for overlapping screeching
+        if (currentScreechStatus != ScreechStatus.Available)
+        {
+            // Debug.Log("Screech is not available");
+            return;
+        }
+
+        // Begin screeching
+        OnScreechStart();
     }
 
     void OnScreechStart()
     {
-        MoveTowardsPoint mtp = screechEffectVP.AddComponent<MoveTowardsPoint>();
+        // Update screeching status
+        currentScreechStatus = ScreechStatus.Performing;
 
-        Vector3 destination = new Vector3(0, 0.5f, 0);
-        float duration = 0.15f;
+        // Apply Screech Effect Volume effects
+        MoveTowardsPoint mtp = screechEffectVolume.AddComponent<MoveTowardsPoint>();
+        mtp.SetDestination(playerCamera.transform.localPosition, screechEffectAppearanceSpeed);
 
-        mtp.SetDestination(destination, duration);
-
-        Debug.Log("Screech started");
+        // End screech after specified amount of time
         Invoke("OnScreechEnd", screechDuration);
     }
 
     void OnScreechEnd()
     {
-        MoveTowardsPoint mtp = screechEffectVP.AddComponent<MoveTowardsPoint>();
+        // Update screeching status
+        currentScreechStatus = ScreechStatus.OnCooldown;
 
-        Vector3 destination = new Vector3(0, -0.5f, 0);
-        float duration = 1f;
+        // Remove Screech Effect Volume effects
+        MoveTowardsPoint mtp = screechEffectVolume.AddComponent<MoveTowardsPoint>();
+        mtp.SetDestination(screechEffectDisappearancePos, screechEffectDisappearanceSpeed);
 
-        mtp.SetDestination(destination, duration);
+        // End screech cooldown after specified amount of time
+        Invoke("OnScreechCooldownEnd", screechCooldownDuration);
+    }
 
-        Debug.Log("Screech ended");
+    void OnScreechCooldownEnd()
+    {
+        // Update screeching status
+        currentScreechStatus = ScreechStatus.Available;
+    }
+
+    public enum ScreechStatus
+    {
+        Forbidden,
+        Available,
+        Performing,
+        OnCooldown,
     }
 }
