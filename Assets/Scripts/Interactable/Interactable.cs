79d7c0e6b53f2bc861f7e21cb4a31fdec0d8f9a2
SiogunJH <homarofficial@gmail.com>
2024-06-29 23:16:15 +0200

    Hadeset and new Camera
    Added a 3d model of a headset for wiretapping set.
    Changed rendering of a game to pixelated render texture, because it hides just how awful those 3d models are looking :c

--- Assets/Scripts/Interactable/Interactable.cs
+++ Assets/Scripts/Interactable/Interactable.cs
@@ -4,44 +4,9 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class InteractableBase : MonoBehaviour
+public class Interactable : MonoBehaviour
 {
-    const string BOOL_SHADER_PROPERTY_OUTLINE_ENABLED = "_OutlineEnabled";
-
     public UnityEvent OnInteract;
 
-    [SerializeField]
-    MeshRenderer[] meshRenderers;
-
-    public virtual void Start()
-    {
-        if (meshRenderers == null || meshRenderers.Length == 0) Debug.LogWarning($"Missing [Mesh Renderer] reference in [{gameObject.name}]");
-    }
-
-    public virtual void Focus()
-    {
-        foreach (var meshRenderer in meshRenderers)
-        {
-            MaterialPropertyBlock propertyBlock = new MaterialPropertyBlock();
-            meshRenderer.GetPropertyBlock(propertyBlock);
-            propertyBlock.SetFloat(BOOL_SHADER_PROPERTY_OUTLINE_ENABLED, 1.0f);
-            meshRenderer.SetPropertyBlock(propertyBlock);
-        }
-    }
-
-    public virtual void Unfocus()
-    {
-        foreach (var meshRenderer in meshRenderers)
-        {
-            MaterialPropertyBlock propertyBlock = new MaterialPropertyBlock();
-            meshRenderer.GetPropertyBlock(propertyBlock);
-            propertyBlock.SetFloat(BOOL_SHADER_PROPERTY_OUTLINE_ENABLED, 0.0f);
-            meshRenderer.SetPropertyBlock(propertyBlock);
-        }
-    }
-
-    public virtual void Interact()
-    {
-        OnInteract?.Invoke();
-    }
+    public void Interact() => OnInteract?.Invoke();
 }
