68290ba7bda1e8cd637ca433f3ea673e602b54f3
SiogunJH <115347822+SiogunJH@users.noreply.github.com>
2024-07-16 19:32:12 +0200

    Screech logic revamped (#27)
    Screech is now activated by holding down screech button, instead of pressing it.
    Screech now has several stages.
    Added inhale and exhale audio for charging up the screech and canceling it.
    Swapped high pitch screech audio for a lower piched one.

--- Assets/Scripts/Player/PlayerScreech.cs
+++ Assets/Scripts/Player/PlayerScreech.cs
@@ -1,127 +1,229 @@
 using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Events;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class PlayerScreech : MonoBehaviour
 {
-    [Header("Screech")]
-    [SerializeField] float screechDuration = 0.6f;
-    [SerializeField] float screechCooldownDuration = 0.5f;
-    [SerializeField] float screechMovementModifier = 0.2f;
-    [SerializeField] float screechMouseSensitivityModifier = 0.2f;
-    [SerializeField] float screechShakeMagnitude = 0.1f;
+    [Header("Settings")]
+    [SerializeField] ScreechStatus status = ScreechStatus.Available;
 
-    float screechEffectAppearanceSpeed = 0.2f;
-    float screechEffectDisappearanceSpeed = 1f;
-    Vector3 screechEffectDisappearancePos = new(0, -0.5f, 0);
-    Coroutine screechShakeCoroutine;
+    [Space, SerializeField] float chargeDuration = 0.8f;
+    [SerializeField] float chargeMovementModifier = 0.2f;
+    [SerializeField] float chargeMouseSensitivityModifier = 0.2f;
+    [SerializeField] AudioClip[] chargeAudio;
+    [SerializeField] AudioClip[] chargeCancelAudio;
+    Coroutine chargeCoroutine;
 
-    public ScreechStatus currentScreechStatus = ScreechStatus.Available;
-
-    [Header("Syringe & Repellant")]
-    public static Bird.Type? birdRepelled = null;
-    [SerializeField] LayerMask corridorEntranceLayer;
-
-
-    [Header("Other")]
-    [SerializeField] GameObject screechEffectVolume;
-    [SerializeField] AudioSource screechAudioSource;
+    [Space, SerializeField] float screechOnsetDuration = 0.1f; // should be lesser than screechDuration
+    [Space, SerializeField] float screechDuration = 0.6f;
+    [SerializeField] float screechShakeMagnitude = 0.15f;
     [SerializeField] AudioClip[] screechAudio;
 
-    [Space]
-    [SerializeField] PlayerLook playerLook;
-    [SerializeField] Transform playerCameraHolder;
+    [Space, SerializeField] float fullRechargeDuration = 1f;
+    [SerializeField] float shortRechargeDuration = 0.4f;
 
-    [Space]
-    [SerializeField] PlayerMovement playerMovement;
+    // [Header("Visuals")]
+    (float Old, float Default, float High) chromaticAberrationIntensity = (Old: 0f, Default: 0f, High: 0.3f);
+    (float Old, float Default, float Low, float High) lensDistortionIntensity = (Old: 0f, Default: 0f, Low: 0.2f, High: 0.4f);
+
+    // [Header("Repellant")]
+    public static Bird.Type? birdRepelled = null;
+
+    [Header("References")]
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] VolumeProfile screechEffectVolumeProfile;
 
     void Start()
     {
-        if (screechEffectVolume == null) Debug.LogWarning($"Missing [Screech Effect Volume] reference in {gameObject.name}");
-        if (screechAudioSource == null) Debug.LogWarning($"Missing [Screech Audio Source] reference in {gameObject.name}");
-        if (playerLook == null) Debug.LogWarning($"Missing [Player Look] reference in {gameObject.name}");
-        if (playerMovement == null) Debug.LogWarning($"Missing [Player Movement] reference in {gameObject.name}");
+        // Check if all references are present
+        if (screechEffectVolumeProfile == null) Debug.LogWarning($"Missing [Screech Effect Volume] reference in {gameObject.name}");
+        if (audioSource == null) Debug.LogWarning($"Missing [Audio Source] reference in {gameObject.name}");
+
+        // Reset Volume Profile overrides
+        screechEffectVolumeProfile.TryGet(out LensDistortion lensDistortion);
+        screechEffectVolumeProfile.TryGet(out ChromaticAberration chromaticAberration);
+
+        lensDistortion.intensity.value = lensDistortionIntensity.Default;
+        chromaticAberration.intensity.value = chromaticAberrationIntensity.Default;
     }
 
 
     public void OnScreech(InputAction.CallbackContext context)
     {
-        // Skip multiple callouts
-        if (!context.performed) return;
+        // Start screeching
+        if (context.started)
+        {
+            if (status != ScreechStatus.Available) return;
 
-        // Do not allow for overlapping screeching
-        if (currentScreechStatus != ScreechStatus.Available) return;
-
-        // Begin screeching
-        OnScreechStart();
+            // Begin charging the screech
+            ScreechInputStart();
+        }
+        else if (context.canceled)
+        {
+            ScreechInputEnd();
+        }
     }
 
-    void OnScreechStart()
+    void ScreechInputStart()
     {
-        // Update screeching status
-        currentScreechStatus = ScreechStatus.Performing;
-
-        // Slow down player movement and mouse sensitivity
-        playerMovement.walkSpeedModifier = screechMovementModifier;
-        PlayerLook.mouseSensitivityModifier = screechMouseSensitivityModifier;
-
-        // Apply Screech Effect Volume effects
-        MoveTowardsPoint mtp = screechEffectVolume.AddComponent<MoveTowardsPoint>();
-        mtp.SetDestination(playerCameraHolder.localPosition, screechEffectAppearanceSpeed);
-        screechShakeCoroutine = StartCoroutine(playerLook.ShakeCamera(screechDuration, screechShakeMagnitude));
-
-        // Play screech sound
-        screechAudioSource.SetClipFromPool(screechAudio);
-        screechAudioSource.Play();
-
-        // End screech after specified amount of time
-        Invoke("OnScreechEnd", screechDuration);
-
-        // Attempt a screech attack
-        OnScreechAttack();
+        status = ScreechStatus.Charging;
+        chargeCoroutine = StartCoroutine(ScreechCharge());
     }
 
-    void OnScreechEnd()
+    IEnumerator ScreechCharge()
     {
-        // Update screeching status
-        currentScreechStatus = ScreechStatus.OnCooldown;
+        // Play charging audio
+        audioSource.SetClipFromPool(chargeAudio);
+        audioSource.Play();
 
+        // Get screech volume effects references
+        screechEffectVolumeProfile.TryGet(out LensDistortion lensDistortion);
+
+        // Await end of charge
+        float chargeProgressInPercent;
+        float chargeDurationRemaining = chargeDuration;
+        while (chargeDurationRemaining > 0)
+        {
+            // Update progress variables
+            chargeDurationRemaining -= Time.deltaTime;
+            chargeProgressInPercent = Mathf.Clamp(1 - (chargeDurationRemaining / chargeDuration), 0, 1);
+
+            // Update visuals
+            lensDistortion.intensity.value = lensDistortionIntensity.Low * chargeProgressInPercent;
+
+            // Slow down player movement and mouse sensitivity over time
+            PlayerMovement.walkSpeedModifier = 1 - (1 - chargeMovementModifier) * chargeProgressInPercent;
+            PlayerLook.mouseSensitivityModifier = 1 - (1 - chargeMouseSensitivityModifier) * chargeProgressInPercent;
+
+            // Execute once per frame
+            yield return null;
+        }
+
+        // Adjust
+
+        // Update status
+        status = ScreechStatus.Prepared;
+    }
+
+    void ScreechInputEnd()
+    {
+        if (status == ScreechStatus.Charging) ScreechCancel();
+        else if (status == ScreechStatus.Prepared) StartCoroutine(ScreechPerform());
+    }
+
+    void ScreechCancel()
+    {
+        // Stop charging
+        StopCoroutine(chargeCoroutine);
+
+        // Play charging audio
+        audioSource.Stop();
+        audioSource.SetClipFromPool(chargeCancelAudio);
+        audioSource.Play();
+
+        // Go on cooldown
+        StartCoroutine(ScreechRecharge(shortRechargeDuration));
+    }
+
+    IEnumerator ScreechPerform()
+    {
+        // Update status
+        status = ScreechStatus.Performing;
+
+        // Play screech audio
+        audioSource.SetClipFromPool(screechAudio);
+        audioSource.Play();
+
+        // Attack
+        ScreechAttack();
+
+        // Shake camera
+        StartCoroutine(PlayerLook.Instance.ShakeCamera(screechDuration, screechShakeMagnitude));
+
+        // Apply screech volume effects
+        screechEffectVolumeProfile.TryGet(out LensDistortion lensDistortion);
+        screechEffectVolumeProfile.TryGet(out ChromaticAberration chromaticAberration);
+
+        // Onset screech effects
+        float screechProgressInPercent;
+        float screechDurationRemaining = screechOnsetDuration;
+        while (screechDurationRemaining > 0)
+        {
+            // Update progress variables
+            screechDurationRemaining -= Time.deltaTime;
+            screechProgressInPercent = Mathf.Clamp(1 - (screechDurationRemaining / screechOnsetDuration), 0, 1);
+
+            // Update visuals
+            lensDistortion.intensity.value = lensDistortionIntensity.Low + (lensDistortionIntensity.High - lensDistortionIntensity.Low) * screechProgressInPercent;
+            chromaticAberration.intensity.value = chromaticAberrationIntensity.High * screechProgressInPercent;
+
+            // Execute once per frame
+            yield return null;
+        }
+
+        screechDurationRemaining = screechDuration - screechOnsetDuration;
+        while (screechDurationRemaining > 0)
+        {
+            // Update progress variables
+            screechDurationRemaining -= Time.deltaTime;
+
+            // Execute once per frame
+            yield return null;
+        }
+
+        // Go on cooldown
+        StartCoroutine(ScreechRecharge(fullRechargeDuration));
+    }
+
+    IEnumerator ScreechRecharge(float rechargeDuration)
+    {
         // Revert player movement speed and mouse sensitivity
-        playerMovement.walkSpeedModifier = 1;
+        PlayerMovement.walkSpeedModifier = 1;
         PlayerLook.mouseSensitivityModifier = 1;
 
-        // Remove Screech Effect Volume effects
-        MoveTowardsPoint mtp = screechEffectVolume.AddComponent<MoveTowardsPoint>();
-        mtp.SetDestination(screechEffectDisappearancePos, screechEffectDisappearanceSpeed);
+        // Apply screech volume effects
+        screechEffectVolumeProfile.TryGet(out LensDistortion lensDistortion);
+        screechEffectVolumeProfile.TryGet(out ChromaticAberration chromaticAberration);
 
-        // End screech cooldown after specified amount of time
-        Invoke("OnScreechCooldownEnd", screechCooldownDuration);
+        // Backup visuals
+        lensDistortionIntensity.Old = lensDistortion.intensity.value;
+        chromaticAberrationIntensity.Old = chromaticAberration.intensity.value;
+
+        // Await end of recharge
+        float rechargeProgressInPercent;
+        float rechargeDurationRemaining = rechargeDuration;
+        while (rechargeDurationRemaining > 0)
+        {
+            // Update progress variables
+            rechargeDurationRemaining -= Time.deltaTime;
+            rechargeProgressInPercent = Mathf.Clamp(1 - (rechargeDurationRemaining / rechargeDuration), 0, 1);
+
+            // Update visuals
+            lensDistortion.intensity.value = lensDistortionIntensity.Old + (lensDistortionIntensity.Default - lensDistortionIntensity.Old) * rechargeProgressInPercent;
+            chromaticAberration.intensity.value = chromaticAberrationIntensity.Old + (chromaticAberrationIntensity.Default - chromaticAberrationIntensity.Old) * rechargeProgressInPercent;
+
+            // Execute once per frame
+            yield return null;
+        }
+
+        // Update status
+        status = ScreechStatus.Available;
     }
 
-    void OnScreechCooldownEnd()
-    {
-        // Update screeching status
-        currentScreechStatus = ScreechStatus.Available;
-    }
-
-    void OnScreechAttack()
+    void ScreechAttack()
     {
         if (birdRepelled == null) return;
 
-        Ray ray = new Ray(playerLook.transform.position, playerLook.transform.forward);
+        Ray ray = new Ray(PlayerLook.Instance.transform.position, PlayerLook.Instance.transform.forward);
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, 10, corridorEntranceLayer))
+        if (Physics.Raycast(ray, out hit, 10))
         {
             var corridorEntrance = hit.collider.gameObject.GetComponent<CorridorEntrance>();
-            if (corridorEntrance == null)
-            {
-                Debug.LogError("An object of [CorridorEntrance] layer with no [CorridorEntrance] script was hit.");
-                return;
-            }
+            if (corridorEntrance == null) return;
 
             var vulnerableOccupants = corridorEntrance.connectedRoom.GetOccupants().Where(bird => bird.birdType == birdRepelled).ToList();
             if (vulnerableOccupants.Count != 0)
@@ -131,11 +233,13 @@ public class PlayerScreech : MonoBehaviour
         birdRepelled = null;
     }
 
-    public enum ScreechStatus
+    enum ScreechStatus
     {
-        Forbidden,
+        Unavailable,
         Available,
+        Charging,
+        Prepared,
         Performing,
-        OnCooldown,
+        Recharging,
     }
 }
